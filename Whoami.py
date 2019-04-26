4465b52cfcbec8cf442234acb3ce938f0cc20d51
Guga Neri <guganeriss@gmail.com>
2019-04-26 13:56:00 -0300

    ajusts

--- Whoami.py
+++ Whoami.py
@@ -5,11 +5,11 @@ import platform, socket, sys
 so = platform.system()
 if so == 'Windows':
     print("Windows")
-    print('Nome do computador:',platform.node())
-    print('Processador:',platform.machine())
-    print('Versão:',platform.platform())
+    print('Nome do computador:', platform.node())
+    print('Processador:', platform.machine())
+    print('Versão:', platform.platform())
     print('#'*50)
-    print('Informações adicionais SO:',platform.win32_ver())
+    print('Informações adicionais SO:', platform.win32_ver())
     for ports in range(1,65535):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #if s.connect_ex((sys.argv[1], ports)) == 0:
@@ -18,10 +18,10 @@ if so == 'Windows':
 
 elif so == 'Linux':
     print("Linux")
-    print('Nome do computador:',platform.node())
-    print('Processador:',platform.machine())
-    print('Versão:',platform.platform())
-    print('Distr: ',platform.linux_distribution())
+    print('Nome do computador:', platform.node())
+    print('Processador:', platform.machine())
+    print('Versão:', platform.platform())
+    print('Distr: ', platform.linux_distribution())
     for ports in range(1,65535):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if s.connect_ex((sys.argv[1], ports)) == 0:
