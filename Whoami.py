1a94dd20c8a25b0a9742aa4cb9ebe960d2d14a38
Guga Neri <guganeriss@gmail.com>
2019-04-26 15:54:38 -0300

    windows add

--- Whoami.py
+++ Whoami.py
@@ -14,13 +14,28 @@ if so == 'Windows':
     print('Adictional Inf:', platform.win32_ver())
     print('Host-IP:', myip)
 
+    #Portas OPEN
+    print('Entrou')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    target = myip
+    print('target ', target)
+    def pscan(port):
+        try:
+            con = s.connect((target, port))
+            return True
+        except:
+            return False
+    for x in range(50000):
+        if pscan(x):
+            print('Port', x, 'is open')
+        else:
+            print('Port', x, 'Closed')
+
+
+    print('Saiu')
     #Verificação de portas em funcionamento
 
-    for ports in range(1,65535):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #if s.connect_ex((sys.argv[myip], ports)) == 0:
-        #    print("Porta", ports, "Aberta")
-        #    s.close()
+
 
 elif so == 'Linux':
     print("SO: Linux")
