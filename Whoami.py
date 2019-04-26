dd712a6ab1547696096524ae1895a8e74869dde7
Guga Neri <guganeriss@gmail.com>
2019-04-26 13:53:14 -0300

    Verific ports

--- Whoami.py
+++ Whoami.py
@@ -1,4 +1,5 @@
-import platform
+import platform, socket, sys
+
 
 #Verificação do SO
 so = platform.system()
@@ -9,13 +10,30 @@ if so == 'Windows':
     print('Versão:',platform.platform())
     print('#'*50)
     print('Informações adicionais SO:',platform.win32_ver())
+    for ports in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #if s.connect_ex((sys.argv[1], ports)) == 0:
+        #    print("Porta", ports, "Aberta")
+        #    s.close()
+
 elif so == 'Linux':
     print("Linux")
     print('Nome do computador:',platform.node())
     print('Processador:',platform.machine())
     print('Versão:',platform.platform())
     print('Distr: ',platform.linux_distribution())
+    for ports in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if s.connect_ex((sys.argv[1], ports)) == 0:
+            print("Porta", ports, "Aberta")
+            s.close()
 else:
     print("Desconhecido")
     print(platform.node())
     print(platform.machine())
+
+#Open Ports Windows SO
+
+
+
+
