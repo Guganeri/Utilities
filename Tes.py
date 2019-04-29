ab592009b0d283c5be92551355450cae6c98ed5a
Guga Neri <guganeriss@gmail.com>
2019-04-29 16:02:16 -0300

    mod

--- Tes.py
+++ Tes.py
@@ -1,53 +1,11 @@
-import socket
-import sys
-from datetime import datetime
+import socket,sys
 
-# Clear the screen
-from pip._vendor.distlib.compat import raw_input
+for ports in range(1,65535):
 
-#subprocess.call('clear', shell=True)
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Ask for input
-remoteServer    = raw_input("Enter a remote host to scan: ")
-remoteServerIP  = socket.gethostbyname(remoteServer)
+                        if s.connect_ex((sys.argv[1], ports)) == 0:
 
-# Print a nice banner with information on which host we are about to scan
-print("-" * 60)
-print("Please wait, scanning remote host", remoteServerIP)
-print("-" * 60)
+                                   print (f'PORTA”, {ports},  “ABERTA')
 
-# Check what time the scan started
-t1 = datetime.now()
-
-# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
-
-# We also put in some error handling for catching errors
-
-try:
-    for port in range(1,65535):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((remoteServerIP, port))
-        if result == 0:
-            print("Port {}: 	 Open".format(port))
-        sock.close()
-
-except KeyboardInterrupt:
-    print("You pressed Ctrl+C")
-    sys.exit()
-
-except socket.gaierror:
-    print('Hostname could not be resolved. Exiting')
-    sys.exit()
-
-except socket.error:
-    print("Couldn't connect to server")
-    sys.exit()
-
-# Checking the time again
-t2 = datetime.now()
-
-# Calculates the difference of time, to see how long it took to run the script
-total =  t2 - t1
-
-# Printing the information to screen
-print('Scanning Completed in: ', total)
+                                   s.close()
--- Whoami.py
+++ Whoami.py
@@ -48,19 +48,19 @@ if so == 'Windows':
     remoteServerIP = socket.gethostbyname(remoteServer)
 
     # Print a nice banner with information on which host we are about to scan
-    print("=-" * 60)
-    print("Please wait, scanning remote host", remoteServerIP)
-    print("=-" * 60)
+    print("=-" * 50)
+    print("Please wait, scanning select host", remoteServerIP)
+    print("=-" * 50)
 
     # Check what time the scan started
-    t1 = datetime.now()
+
 
     # Using the range function to specify ports (here it will scans all ports between 1 and 1024)
 
     # We also put in some error handling for catching errors
 
     try:
-        for port in range(1, 65535):
+        for port in range(1, 500):
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             result = sock.connect_ex((remoteServerIP, port))
             if result == 0:
@@ -79,16 +79,8 @@ if so == 'Windows':
         print("Couldn't connect to server")
         sys.exit()
 
-    # Checking the time again
-    t2 = datetime.now()
-
-    # Calculates the difference of time, to see how long it took to run the script
-    total = t2 - t1
-
-    # Printing the information to screen
-    print('Scanning Completed in: ', total)
-
     #Finish script for pythonforbeginners
+    #Need optimized
 
     #Ports OPEN
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
