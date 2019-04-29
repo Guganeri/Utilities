02b23fc8e1c910a30d7102d616cd4eaf946a90c7
Guga Neri <guganeriss@gmail.com>
2019-04-29 16:46:39 -0300

    add linux port scan

--- Whoami.py
+++ Whoami.py
@@ -53,7 +53,6 @@ if so == 'Windows':
 
     # Check what time the scan started
 
-
     # Using the range function to specify ports (here it will scans all ports between 1 and 1024)
 
     # We also put in some error handling for catching errors
@@ -93,21 +92,43 @@ elif so == 'Linux':
     target = myip
     print('Target ', target)
 
+    # Script by pythonforbeginners Custom for Gustavo Neri
 
-    def pscan(port):
-        try:
-            s.connect((target, port))
-            return True
-        except:
-            return None
+    remoteServer = raw_input("Enter a remote host to scan or you Host-Ip: ")
+    remoteServerIP = socket.gethostbyname(remoteServer)
 
+    # Print a nice banner with information on which host we are about to scan
+    print("=-" * 50)
+    print("Please wait, scanning select host", remoteServerIP)
+    print("=-" * 50)
 
-    for x in range(0, 65536):
-        if pscan(x):
-            print('Port', x, 'is open')
-        # else:
-        #    print('Port', x, 'Closed')
-    print('=-' * 50)
+    # Check what time the scan started
+
+    # Using the range function to specify ports (here it will scans all ports between 1 and 1024)
+
+    # We also put in some error handling for catching errors
+
+    try:
+        for port in range(1, 500):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((remoteServerIP, port))
+            if result == 0:
+                print("Port {}: 	 Open".format(port))
+            sock.close()
+
+    except KeyboardInterrupt:
+        print("You pressed Ctrl+C")
+        sys.exit()
+
+    except socket.gaierror:
+        print('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    except socket.error:
+        print("Couldn't connect to server")
+        sys.exit()
+
+    # Need optimized
 
 else:
     print("Desconhecido")
