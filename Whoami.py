bd770976ae4b98c16a9d3214d60ff7e1d5e89d56
Guga Neri <guganeriss@gmail.com>
2019-04-29 14:57:55 -0300

    update script

--- Whoami.py
+++ Whoami.py
@@ -1,4 +1,9 @@
-import platform, socket, sys
+import platform
+import socket
+import sys
+import datetime
+from pip._vendor.distlib.compat import raw_input
+
 """
  [] Script for devops
  [] Checking ports in use and opening ports via command line
@@ -26,6 +31,7 @@ myip = socket.gethostbyname(socket.gethostname())
 #Starting Utility
 if so == 'Windows':
     print('=-' * 50)
+    print('\n')
     print("SO: Windows")
     print('HostName:', platform.node())
     #Arquiterura do processador
@@ -33,7 +39,56 @@ if so == 'Windows':
     print('Version:', platform.platform())
     print('Adictional Inf:', platform.win32_ver())
     print('Host-IP:', myip)
+    print('\n')
     print('=-' *50)
+
+    #Script by pythonforbeginners Custom for Gustavo Neri
+
+    remoteServer = raw_input("Enter a remote host to scan or you Host-Ip: ")
+    remoteServerIP = socket.gethostbyname(remoteServer)
+
+    # Print a nice banner with information on which host we are about to scan
+    print("=-" * 60)
+    print("Please wait, scanning remote host", remoteServerIP)
+    print("=-" * 60)
+
+    # Check what time the scan started
+    t1 = datetime.now()
+
+    # Using the range function to specify ports (here it will scans all ports between 1 and 1024)
+
+    # We also put in some error handling for catching errors
+
+    try:
+        for port in range(1, 65535):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((remoteServerIP, port))
+            if result == 0:
+                print("Port {}: 	 Open".format(port))
+            sock.close()
+
+    except KeyboardInterrupt:
+        print("You pressed Ctrl+C")
+        sys.exit()
+
+    except socket.gaierror:
+        print('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    except socket.error:
+        print("Couldn't connect to server")
+        sys.exit()
+
+    # Checking the time again
+    t2 = datetime.now()
+
+    # Calculates the difference of time, to see how long it took to run the script
+    total = t2 - t1
+
+    # Printing the information to screen
+    print('Scanning Completed in: ', total)
+
+    #Finish script for pythonforbeginners
 
     #Ports OPEN
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
