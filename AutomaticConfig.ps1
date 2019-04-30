d4c69badb1eed408eb821c0cf5ed0023f005a439
GUGA NERI <guganeriss@gmail.com>
2019-04-30 10:33:43 -0300

    add

--- AutomaticConfig.ps1
+++ AutomaticConfig.ps1
@@ -1,2 +1,21 @@
+ipmo international #import module internacional config
+
+#Exibindo informações sobre locaidade
+Get-WinSystemLocale
+#Definindo região
+PS C:\>Set-WinSystemLocale -SystemLocale ja-JP
+
+
+
+
 #Automatic Config 1.0
 Get-Command *-Service
+
+#Lista Serviços Rodando
+get-service | where-object { $_.Status -eq "running" }
+
+#Lista Serviço Especifico: Exemplo "aut"
+get-service | where-object { $_.Name -eq "aut" }
+
+#Iniciando Serviço
+start-service aut
