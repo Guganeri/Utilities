de617c5e2f9cd3196b0315436af5193cee6a29da
GUGA NERI <guganeriss@gmail.com>
2019-04-30 10:52:22 -0300

    add

--- AutomaticConfig.ps1
+++ AutomaticConfig.ps1
@@ -4,9 +4,8 @@ ipmo international #import module internacional config
 Get-WinSystemLocale
 #Definindo região
 PS C:\>Set-WinSystemLocale -SystemLocale ja-JP
-
-
-
+#Iniciando um serviço setando o caminho
+Start-Process -Filepath "C:\Program Files (x86) GoogleChromeApplicationchrome.exe"
 
 #Automatic Config 1.0
 Get-Command *-Service
