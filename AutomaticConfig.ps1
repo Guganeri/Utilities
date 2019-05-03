ba2e15ce2e44fda3fdef07e5e8991b8cc1e62813
Guga Neri <guganeriss@gmail.com>
2019-05-03 14:31:45 -0300

    Ajustando Funcoes

--- AutomaticConfig.ps1
+++ AutomaticConfig.ps1
@@ -7,8 +7,8 @@ function Show-Menu {
     cls
     Write-Host "========================= $Title ========================="
 
-    Write-Host "1 - Para criar usuario ADM"
-    Write-Host "2 - Configurar Ambiente"
+    Write-Host "1 - Configurar Ambiente"
+    Write-Host "2 - Para criar usuario ADM"
     Write-Host "Q - Para Sair"
 }
 do {
@@ -17,48 +17,45 @@ do {
     switch ($input) {
         '1' {
             cls
-            CriarUsuario
+            ################## DATA - HORA ##################
+            ## Definindo Horario padrão como o de Brasilia
+            tzutil /s "E. South America Standard Time"
+            ## Definindo formato da data
+            $culture = Get-Culture
+            $culture.DateTimeFormat.ShortDatePattern = 'dd/MM/yyyy'
+            Set-Culture $culture
+            Set-Culture 'pt-BR'
+            $culture.DateTimeFormat.ShortTimePattern = 'HH:mm'
+            #$culture.DateTimeFormat.ShortDatePattern =
+
+            #Configuração de Lingaguem
+            $currentlist = Get-WinUserLanguageList
+            #Forçando o uso da linguagem
+            Set-WinUserLanguageList pt-BR -Force
+            $currentlist | ForEach-Object { if (($_.LanguageTag -ne "pt-BR") -and ($_.LanguageTag -ne "pt-BR")) { exit } }
+
+            #Configurações WinServer
+            #Exibindo informações sobre locaidade
+            Get-WinSystemLocale #OK
+            #Definindo região
+            Set-WinSystemLocale -SystemLocale pt-BR
+            #Verificar as configura��es - Descomentar a linha
+            #get-culture | Format-List -Property *   
+            
         } '2' {
             cls
-            ConfigurarAmbiente
+            #Adicionar Usuario
+            net user cdtlabiesp cdtlabiesp /passwordchg:yes /add
+            #Permissão de Administrador
+            net localgroup administrators cdtlabiesp /add
+
+            #Reiniciar
+            shutdown /r  
+            Write-Host "Cancelar? CMD -> shutdown /a"                   
         }'q' {
+            Write-Host "Thx"
             return
         }
     }
     Pause
 }until($input -eq 'q')
-function ConfigurarAmbiente {
-    ################## DATA - HORA ##################
-    ## Definindo Horario padrão como o de Brasilia
-    tzutil /s "E. South America Standard Time"
-    ## Definindo formato da data
-    $culture = Get-Culture
-    $culture.DateTimeFormat.ShortDatePattern = 'dd/MM/yyyy'
-    Set-Culture $culture
-    Set-Culture 'pt-BR'
-    $culture.DateTimeFormat.ShortTimePattern = 'HH:mm'
-    #$culture.DateTimeFormat.ShortDatePattern =
-
-    #Configuração de Lingaguem
-    $currentlist = Get-WinUserLanguageList
-    #Forçando o uso da linguagem
-    Set-WinUserLanguageList pt-BR -Force
-    $currentlist | ForEach-Object { if (($_.LanguageTag -ne "pt-BR") -and ($_.LanguageTag -ne "pt-BR")) { exit } }
-
-    #Configurações WinServer
-    #Exibindo informações sobre locaidade
-    Get-WinSystemLocale #OK
-    #Definindo região
-    Set-WinSystemLocale -SystemLocale pt-BR
-    #Verificar as configura��es - Descomentar a linha
-    #get-culture | Format-List -Property *    
-}
-function CriarUsuario {
-    #Adicionar Usuario
-    net user { usuario } { senhausuario } /passwordchg:yes /add
-    #Permissão de Administrador
-    net localgroup administrators { usuario } /add
-
-    #Reiniciar
-    shutdown /r        
-}
