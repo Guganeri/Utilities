803612d6ca8ae8cf271c2438ce557fe32dc01dd7
Guga Neri <guganeriss@gmail.com>
2019-05-08 16:12:39 -0300

    ajustes download linguagem pt-br

--- TesteDownloadLanguage.ps1
+++ TesteDownloadLanguage.ps1
@@ -1,7 +1,9 @@
 ######## Download de pt-BR Teste via powershell ############
 $url = "https://www.microsoft.com/pt-br/download/confirmation.aspx?id=9490"
-$output = "C:\Users\gustavo.silva.ext\Desktop\Utilities"
+$output = "C:\Users\"
 $start_time = Get-Date
 
 Invoke-WebRequest -Uri $url -OutFile $output
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+Start-Process -FilePath "C:\Users\"
