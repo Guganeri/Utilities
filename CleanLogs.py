2cf6fae0057ceecca5bb029d9a3bb557587de59d
Gustavo Neri <gustavo.neri@conductor.com.br>
2019-07-26 14:37:24 -0300

    Teste

--- CleanLogs.py
+++ CleanLogs.py
@@ -5,19 +5,14 @@ import os
 verification = 3600
 
 while True:
-    print (datetime.now())
+    print (f ,' Ultima verificação em: {datetime.now()}')
     sleep(verification)
-
-try:
-    os.mkdir('Teste0001')
-    print('Diretorio criado com sucesso')
-except OSError:
-    print('Diretorio não criado')
-
-print(os.listdir(os.getcwd()))
-
-
-#os.remove("arquivo.txt")
-os.removedirs("Teste0001")
-print(os.listdir(os.getcwd()))
-
+    try:
+        os.mkdir('Teste0001')
+        print('Diretorio criado com sucesso')
+    except OSError:
+        print('Diretorio não criado')
+    print(os.listdir(os.getcwd()))
+    os.removedirs("Teste0001")
+    print(os.listdir(os.getcwd()))
+    #os.remove("arquivo.txt")
