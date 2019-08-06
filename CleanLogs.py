dc744fa6aa26b8c77faf441af1feec6cc19aa171
Gustavo Neri <gustavo.neri@conductor.com.br>
2019-08-06 10:48:42 -0300

    CleanLogs

--- CleanLogs.py
+++ CleanLogs.py
@@ -2,8 +2,8 @@ from time import sleep
 from datetime import datetime
 import os
 
-verification = 3600
-
+#verification = 3600
+verification = 60
 while True:
     print(datetime.now())
     sleep(verification)
@@ -11,9 +11,14 @@ while True:
         os.mkdir('Teste0001')
         print('Diretorio criado com sucesso')
     except OSError:
-        print(OSError   )
+        print(OSError)
 
-    print(os.listdir(os.getcwd()))
-    os.removedirs("Teste0001")
+    #print(os.listdir(os.getcwd()))
+    try:
+        os.removedirs("Teste0001")
+        print('Diretorio removido com sucesso !!! ')
+    except OSError:
+        print(OSError)
+
     print(os.listdir(os.getcwd()))
     #os.remove("arquivo.txt")
